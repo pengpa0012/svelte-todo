1f97ec4a1ed7c45f8fc966ab81184f4993428772
Pengpa0012 <peng.ilikha@gmail.com>
2023-11-22 06:28:42 +0800

    added create, delete todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  let todoText = ""
+  let todos = ["todo 1", "todo 2", "todo 3"]
+
+  function addTodo () {
+    if (!todoText) return
+    todos.push(todoText)
+    todos = todos
+    todoText = ""
+  }
+
+  function deleteTodo (id: number) {
+    todos.splice(id, 1)
+    todos = todos
+  } 
+</script>
+
+<div class="container">
+  <h1 class="header">Todo</h1>
+  <div class="flex">
+    <input type="text" placeholder="Add Todo..." bind:value={todoText}>
+    <button on:click={addTodo}>Add</button>
+  </div>
+  <ul class="todos">
+    {#each todos as todo, i}
+      <li>{todo} <button on:click={() => deleteTodo(i)}>Delete</button></li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  
+
+  .container {
+    font-family: sans-serif;
+    max-width: 600px;
+    margin: 2rem auto;
+  }
+
+  .flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .flex input {
+    flex: 2;
+    padding: 5px;
+  }
+
+  .flex button {
+    padding: 5px 8px;
+  }
+
+  .header {
+    text-align: center;
+    font-size: 2rem;
+  }
+
+  .todos {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+  }
+  
+</style>
